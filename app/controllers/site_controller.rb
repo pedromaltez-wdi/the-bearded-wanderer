f5e2a026f0053f7adf8d84acec8f481cfe1f879e
Pedro Maltez <maltezpedro@gmail.com>
2013-05-15 10:57:16 +0100

    Add User login error catches

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,21 +1,22 @@
 class SiteController < ApplicationController
   def index
-    @user = User.new
+    @new_user = User.new
     @users = User.all
   end
 
 
   def login
-    @user = User.find_by(email: params[:email])
-
+    begin
+      @user = User.find_by(email: params[:email])
       if @user and @user.authenticate(params[:password])
         session[:user_id] = @user.id
         redirect_to root_url
-
       else
-        flash.now.alert = 'Oops'
-        render :index
+        redirect_to root_url, notice: 'Email or password incorrect!'
       end
+    rescue Exception => e
+      redirect_to root_url, notice: 'Please register!'
+    end
   end
 
   def logout
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 TheBeardedWanderer::Application.routes.draw do
   resources :users, :except => [ :new, :edit ], defaults: {format: :json}
   
-  get 'home' => 'site#home'
+  # get 'home' => 'site#home'
   post '/login' => 'site#login'
   get '/logout' => 'site#logout'
   root :to => 'site#index'
