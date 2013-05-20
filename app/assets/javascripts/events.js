72d71b17fd179063ff726da8c4cac4b655a0085c
Pedro Maltez <maltezpedro@gmail.com>
2013-05-20 19:28:30 +0100

    First attempt droppable

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -2,11 +2,49 @@
 // All this logic will automatically be available in application.js.
 
 $(function() {
-  $( ".meet" ).draggable();
+  // $( ".meet" ).draggable();
 
   var eventTimes = $('.event-time')
   for (i = 0; i < eventTimes.length; ++i) {
     var eventTime = parseInt($(eventTimes[i]).text());
     $(eventTimes[i]).html(moment(eventTime).format('DD/MM/YYYY, H:mm'));
   };
+
+  var adjustment
+
+  $("ol.simple_with_animation").sortable({
+    group: 'simple_with_animation',
+    pullPlaceholder: false,
+    // animation on drop
+    onDrop: function  (item, targetContainer, _super) {
+      var clonedItem = $('<li/>').css({height: 0})
+      item.before(clonedItem)
+      clonedItem.animate({'height': item.height()})
+      
+      item.animate(clonedItem.position(), function  () {
+        clonedItem.detach()
+        _super(item)
+      })
+    },
+
+    // set item relative to cursor position
+    onDragStart: function ($item, container, _super) {
+      var offset = $item.offset(),
+      pointer = container.rootGroup.pointer
+
+      adjustment = {
+        left: pointer.left - offset.left,
+        top: pointer.top - offset.top
+      }
+
+      _super($item, container)
+    },
+    onDrag: function ($item, position) {
+      $item.css({
+        left: position.left - adjustment.left,
+        top: position.top - adjustment.top
+      })
+    }
+  })
+
 });
